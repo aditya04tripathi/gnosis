14412f000083aadefa4d4e268a6ea53a909b8427
Aditya Tripathi <adityatripathi.at04@gmail.com>
2026-09-06 07:33:35 +1000

    refactor: remove OpenTelemetry and Prometheus metrics code
    - Delete instrumentation, tracing helpers, HTTP metrics, and /api/metrics

--- app/api/metrics/route.ts
+++
@@ -1,9 +0,0 @@
-import { NextResponse } from "next/server";
-import { register as metricsRegister } from "@/lib/http-metrics";
-
-export async function GET() {
-	const metrics = await metricsRegister.metrics();
-	return new NextResponse(metrics, {
-		headers: { "Content-Type": metricsRegister.contentType },
-	});
-}
